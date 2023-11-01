03d202142f4f9ccf535225fdbfb74cc0412319ca
Gustavo Del Razo <1722110326@utectulancingo.edu.mx>
2023-11-01 14:36:02 +0000

    ya2

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,36 +1,29 @@
-function getAll(){
-    var request = new XMLHttpRequest;
-    //request.open('GET',"http://localhost:8000/contactos");
-    request.open('GET',"https://api-contactos-backend1-785dfc9292d6.herokuapp.com");
+function getAll() {
+    var request = new XMLHttpRequest();
+    request.open('GET', "https://api-contactos-backend1-785dfc9292d6.herokuapp.com");
     request.send();
 
     request.onload = (e) => {
         const response = request.responseText;
         const json = JSON.parse(response);
-        console.log("response: " + response);
-        console.log("json: " + json);
-        console.log("status_code: " + request.status);
-
-        console.log("Email: " + json[0]["email"]);
-        console.log("Nombre: " + json[0]["nombre"]);
-        console.log("Telefono: " + json[0]["telefono"]);
 
         const tbody_contactos = document.getElementById("tbody_contactos");
-    
-        var tr = document.createElement("tr");
-        var td_email = document.createElement("td");
-        var td_nombre = document.createElement("td");
-        var td_telefono = document.createElement("td");
 
-        td_email.innerHTML = json[1]["email"];
-        td_nombre.innerHTML = json[1]["nombre"];
-        td_telefono.innerHTML = json[1]["telefono"];
+        for (let i = 0; i < json.length; i++) {
+            var tr = document.createElement("tr");
+            var td_email = document.createElement("td");
+            var td_nombre = document.createElement("td");
+            var td_telefono = document.createElement("td");
 
-        tr.appendChild(td_email);
-        tr.appendChild(td_nombre);
-        tr.appendChild(td_telefono);
+            td_email.innerHTML = json[i]["email"];
+            td_nombre.innerHTML = json[i]["nombre"];
+            td_telefono.innerHTML = json[i]["telefono"];
 
-        tbody_contactos.appendChild(tr);
+            tr.appendChild(td_email);
+            tr.appendChild(td_nombre);
+            tr.appendChild(td_telefono);
 
+            tbody_contactos.appendChild(tr);
+        }
     };
-};
+}
