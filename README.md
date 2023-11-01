7c396069aa6455409466fbd4db99be694beac039
GustavoDelRazo <122810250+GustavoDelRazo@users.noreply.github.com>
2023-11-01 07:30:00 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# api_contactos_frontend
+# heroku_frontend
+Heroku Frontend
+
+gunicorn  --bind 0.0.0.0:8080 app:app
+
+twistd -n web --path httpdocs
+
+https://shm-frontend-c3f2dc0fa89c.herokuapp.com/
+
+https://shm-frontend-c3f2dc0fa89c.herokuapp.com/
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+
+@app.route("/buscar")
+def buscar():
+    return render_template('buscar.html')
