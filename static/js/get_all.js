335e7e40898ae1ceb0dfb84e4e9a81c5d12e0a38
Gustavo Del Razo <1722110326@utectulancingo.edu.mx>
2023-11-09 16:29:26 +0000

    ya

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,6 +1,6 @@
 function getAll() {
     var request = new XMLHttpRequest();
-    request.open('GET', "https://8000-gustavodelr-apicontacto-ped2bjym73u.ws-us106.gitpod.io/");
+    request.open('GET', "https://8000-gustavodelr-apicontacto-ped2bjym73u.ws-us106.gitpod.io");
     request.send();
 
     request.onload = (e) => {
