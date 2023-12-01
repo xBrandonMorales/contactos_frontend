3d7736a2e5866417476d8054932645350b128748
Brandon Castro Morales <adrimora.0232@gmail.com>
2023-12-01 23:32:42 +0000

    gg

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,66 +1,77 @@
 function getAll() {
     var request = new XMLHttpRequest();
-    request.open('GET', "https://8000-xbrandonmor-contactosba-865ied0q764.ws-us106.gitpod.io/contactos");
+
+    request.onerror = function () {
+        console.error("Error al realizar la solicitud.");
+    };
+
+    request.open('GET', "https://8080-xbrandonmor-contactosba-865ied0q764.ws-us106.gitpod.io", true);
     request.send();
 
     request.onload = (e) => {
-        const response = request.responseText;
-        const json = JSON.parse(response);
-        console.log("response: " + response);
-        console.log("json: " + JSON.stringify(json));
-        console.log("status_code: " + request.status);
+        if (request.status === 200) {
+            try {
+                const response = request.responseText;
+                const json = JSON.parse(response);
 
-        const tbody_contactos = document.getElementById("tbody_contactos");
+                const tbody_contactos = document.getElementById("tbody_contactos");
 
-        // Limpiar cualquier contenido previo en la tabla
-        tbody_contactos.innerHTML = "";
+                // Limpiar cualquier contenido previo en la tabla
+                tbody_contactos.innerHTML = "";
 
-        json.forEach((contact) => {
-            var tr = document.createElement("tr");
-            var td_email = document.createElement("td");
-            var td_nombre = document.createElement("td");
-            var td_telefono = document.createElement("td");
-            var td_options = document.createElement("td");
+                json.forEach((contact) => {
+                    var tr = document.createElement("tr");
+                    var td_email = document.createElement("td");
+                    var td_nombre = document.createElement("td");
+                    var td_telefono = document.createElement("td");
+                    var td_options = document.createElement("td");
 
-            td_email.innerHTML = contact["email"];
-            td_nombre.innerHTML = contact["nombre"];
-            td_telefono.innerHTML = contact["telefono"];
+                    td_email.textContent = contact["email"];
+                    td_nombre.textContent = contact["nombre"];
+                    td_telefono.textContent = contact["telefono"];
 
-            // Crear botón "Ver" para cada registro
-            var viewButton = document.createElement("button");
-            viewButton.textContent = "Ver";
-            viewButton.addEventListener("click", function () {
-                // Redirige a la página "ver.html" con el correo electrónico como parámetro
-                window.location.href = `ver?email=${contact["email"]}`;
-            });
+                    // Crear botón "Ver" para cada registro
+                    var viewButton = document.createElement("button");
+                    viewButton.textContent = "Ver";
+                    viewButton.addEventListener("click", function () {
+                        // Redirige a la página "ver.html" con el correo electrónico como parámetro
+                        window.location.href = `ver?email=${contact["email"]}`;
+                    });
 
-            // Crear botón "Editar" para cada registro
-            var editButton = document.createElement("button");
-            editButton.textContent = "Editar";
-            editButton.addEventListener("click", function () {
-                // Redirige a la página "editar.html" con el correo electrónico como parámetro
-                window.location.href = `editar?email=${contact["email"]}`;
-            });
+                    // Crear botón "Editar" para cada registro
+                    var editButton = document.createElement("button");
+                    editButton.textContent = "Editar";
+                    editButton.addEventListener("click", function () {
+                        // Redirige a la página "editar.html" con el correo electrónico como parámetro
+                        window.location.href = `editar?email=${contact["email"]}`;
+                    });
 
-            // Crear botón "Borrar" para cada registro
-            var deleteButton = document.createElement("button");
-            deleteButton.textContent = "Borrar";
-            deleteButton.addEventListener("click", function () {
-                // Redirige a la página "borrar.html" con el correo electrónico como parámetro
-                window.location.href = `borrar?email=${contact["email"]}`;
-            });
+                    // Crear botón "Borrar" para cada registro
+                    var deleteButton = document.createElement("button");
+                    deleteButton.textContent = "Borrar";
+                    deleteButton.addEventListener("click", function () {
+                        // Redirige a la página "borrar.html" con el correo electrónico como parámetro
+                        window.location.href = `borrar?email=${contact["email"]}`;
+                    });
 
-            // Agregar los botones a la celda de opciones
-            td_options.appendChild(viewButton);
-            td_options.appendChild(editButton);
-            td_options.appendChild(deleteButton);
+                    // Agregar los botones a la celda de opciones
+                    td_options.appendChild(viewButton);
+                    td_options.appendChild(editButton);
+                    td_options.appendChild(deleteButton);
 
-            tr.appendChild(td_email);
-            tr.appendChild(td_nombre);
-            tr.appendChild(td_telefono);
-            tr.appendChild(td_options);
+                    tr.appendChild(td_email);
+                    tr.appendChild(td_nombre);
+                    tr.appendChild(td_telefono);
+                    tr.appendChild(td_options);
 
-            tbody_contactos.appendChild(tr);
-        });
+                    tbody_contactos.appendChild(tr);
+                });
+
+            } catch (error) {
+                console.error("Error al procesar la respuesta:", error);
+            }
+        } else {
+            console.error("Error en la solicitud. Código de estado:", request.status);
+        }
     };
 }
--- static/js/insertar.js
+++ static/js/insertar.js
@@ -1,3 +1,81 @@
+function getAll() {
+    var request = new XMLHttpRequest();
+
+    request.onerror = function () {
+        console.error("Error al realizar la solicitud.");
+    };
+
+    request.open('GET', "https://8080-xbrandonmor-contactosba-865ied0q764.ws-us106.gitpod.io", true);
+    request.send();
+
+    request.onload = (e) => {
+        if (request.status === 200) {
+            try {
+                const response = request.responseText;
+                const json = JSON.parse(response);
+
+                const tbody_contactos = document.getElementById("tbody_contactos");
+
+                // Limpiar cualquier contenido previo en la tabla
+                tbody_contactos.innerHTML = "";
+
+                json.forEach((contact) => {
+                    var tr = document.createElement("tr");
+                    var td_email = document.createElement("td");
+                    var td_nombre = document.createElement("td");
+                    var td_telefono = document.createElement("td");
+                    var td_options = document.createElement("td");
+
+                    td_email.textContent = contact["email"];
+                    td_nombre.textContent = contact["nombre"];
+                    td_telefono.textContent = contact["telefono"];
+
+                    // Crear botón "Ver" para cada registro
+                    var viewButton = document.createElement("button");
+                    viewButton.textContent = "Ver";
+                    viewButton.addEventListener("click", function () {
+                        // Redirige a la página "ver.html" con el correo electrónico como parámetro
+                        window.location.href = `ver?email=${contact["email"]}`;
+                    });
+
+                    // Crear botón "Editar" para cada registro
+                    var editButton = document.createElement("button");
+                    editButton.textContent = "Editar";
+                    editButton.addEventListener("click", function () {
+                        // Redirige a la página "editar.html" con el correo electrónico como parámetro
+                        window.location.href = `editar?email=${contact["email"]}`;
+                    });
+
+                    // Crear botón "Borrar" para cada registro
+                    var deleteButton = document.createElement("button");
+                    deleteButton.textContent = "Borrar";
+                    deleteButton.addEventListener("click", function () {
+                        // Redirige a la página "borrar.html" con el correo electrónico como parámetro
+                        window.location.href = `borrar?email=${contact["email"]}`;
+                    });
+
+                    // Agregar los botones a la celda de opciones
+                    td_options.appendChild(viewButton);
+                    td_options.appendChild(editButton);
+                    td_options.appendChild(deleteButton);
+
+                    tr.appendChild(td_email);
+                    tr.appendChild(td_nombre);
+                    tr.appendChild(td_telefono);
+                    tr.appendChild(td_options);
+
+                    tbody_contactos.appendChild(tr);
+                });
+
+            } catch (error) {
+                console.error("Error al procesar la respuesta:", error);
+            }
+        } else {
+            console.error("Error en la solicitud. Código de estado:", request.status);
+        }
+    };
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const insertForm = document.getElementById("insertForm");
     const insertResultDiv = document.getElementById("insertResult");
@@ -10,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const telefono = document.getElementById("telefono").value;
 
         try {
-            const response = await fetch("https://8080-xbrandonmor-contactosfr-srymps5rnj7.ws-us106.gitpod.io/insertar/", {
+            const response = await fetch("https://8080-xbrandonmor-contactosba-865ied0q764.ws-us106.gitpod.io/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
